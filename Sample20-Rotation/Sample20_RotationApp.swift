04043c38769df108a699779d2051d44c917440ce
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-03-11 16:20:54 +0900

    Initial Commit

---
+++ Sample20-Rotation/Sample20_RotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample20_RotationApp.swift
+//  Sample20-Rotation
+//
+//  Created by keiji yamaki on 2021/03/11.
+//
+
+import SwiftUI
+
+@main
+struct Sample20_RotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
