0cf6cad1699730eea1daf650518b32427df5d365
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-03-11 16:23:30 +0900

    デバイスの傾きを検知

--- Sample20-Rotation/ContentView.swift
+++ Sample20-Rotation/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    @State private var orientation = UIDeviceOrientation.unknown
+
+     var body: some View {
+         Group {
+             if orientation.isPortrait {
+                 Text("Portrait")
+             } else if orientation.isLandscape {
+                 Text("Landscape")
+             } else if orientation.isFlat {
+                 Text("Flat")
+             } else {
+                 Text("Unknown")
+             }
+         }
+         .onRotate { newOrientation in
+             orientation = newOrientation
+         }
+     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
     }
 }
